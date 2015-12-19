1c7cc375a81cf1a67eb9d0b292cf53b07566a3e4
Stuart Bates <stuart@builtbyclick.com>
2015-12-19 19:59:58 +0000

    Load decorators correctly in staging environment

--- lib/spree_product_assembly/engine.rb
+++ lib/spree_product_assembly/engine.rb
@@ -6,7 +6,7 @@ module SpreeProductAssembly
 
     def self.activate
       Dir.glob(File.join(File.dirname(__FILE__), "../../app/**/*_decorator.rb")) do |c|
-        Rails.env.production? ? require(c) : load(c)
+        Rails.env.development? ? load(c) : require(c)
       end
     end
 
